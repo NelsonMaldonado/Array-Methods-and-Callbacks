25c0af66773c50e09934bd16e2987a5896a3e74b
Nelson Maldonado <play2nino@gmail.com>
2021-06-15 20:07:47 -0700

    task 4 done

--- index.js
+++ index.js
@@ -27,10 +27,12 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
+
+function getFinals(data) {
+    return (data.filter(worldCup=> worldCup.Stage === 'Final'));
    /* code here */
 }
-
+console.log ('Task 2', getFinals(fifaData));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -39,8 +41,12 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
+function getYears(array,getFinalsC) {
     /* code here */
+    const years = getFinalsC(array).map(function(item){
+        return item.Year;
+    });
+    return years;
 }
 
 
